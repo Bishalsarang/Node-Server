f8c1464691a5c24ab3e92cba2d86b86f171af18f
Bishalsarang <bishalsarang@gmail.com>
2020-08-10 23:16:11 +0545

    Add server that uses PORT number from .env

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const http = require('http');
+const PORT = process.env.PORT;
 
+console.log(PORT);
 const server = http.createServer((request, response) => {});
 
-server.listen(3001);
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
