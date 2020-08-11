3aa950a97062e2130e98cb02b6db4fed241ebaa9
Bishalsarang <bishalsarang@gmail.com>
2020-08-11 07:42:37 +0545

    Complete write, read, rename and delete

--- src/fileOperation.js
+++ src/fileOperation.js
@@ -18,49 +18,34 @@ const rename = (filePath, newFilePath) => {
   });
 };
 
-// const delete = (filePath) => {
-// return new Promise((resolve, reject) => {
-//   fs.(filePath, newFilePath, (err, done) => (err ? reject(err) : resolve(done)));
-// });
-// };
-
-/**
- *
- *@param {String} mode Mode to open file: read, write or append
- * @param {String} filePath
- * @param {String} content
- */
-const open = (filePath, mode, content = '', encoding = 'UTF-8') => {
-  switch (mode.toLowerCase()) {
-    case 'read':
-      read(filePath, encoding)
-        .then((data) => console.log('Succesfully read', data))
-        .catch((err) => console.log('Error', err));
-      break;
-    case 'write':
-      write(filePath, content)
-        .then((data) => console.log(`Successfully written ${filePath}`))
-        .catch((err) => console.log('Error', err));
-      break;
-    case 'rename':
-      rename(filePath, './newOut.txt')
-        .then(() => console.log('Successfully renamed'))
-        .catch((err) => console.log('Error', err));
-      break;
-    case 'append':
-      console.log('append');
-      break;
-    case 'delete':
-      console.log('delete');
-      break;
-
-    default:
-      console.log('Operation not found');
-  }
+const del = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.unlink(filePath, (err, done) => (err ? reject(err) : resolve(done)));
+  });
 };
 
-// open('./out.txt', 'write', 'hello');
-// open('./out.txt', 'read');
-open('./out.txt', 'rename', './newOut.txt');
-console.log('DO stg');
-module.exports = { open, read, write, rename };
+// write('./a.txt', 'Hello Node')
+//   .then(() => {
+//     console.log('File successfully written');
+//   })
+//   .catch((err) => console.log('Failed to write file', err));
+
+// read('./a.txt')
+//   .then((data) => {
+//     console.log('File successfully read: ', data);
+//   })
+//   .catch((err) => console.log('Failed to read file', err));
+
+// rename('./a.txt', './b.txt')
+//   .then(() => {
+//     console.log('File successfully renamed');
+//   })
+//   .catch((err) => console.log('Failed to rename file', err));
+
+// del('./b.txt')
+//   .then(() => {
+//     console.log('File successfully deleted');
+//   })
+//   .catch((err) => console.log('Failed to delete file', err));
+
+module.exports = { read, write, rename, del };
