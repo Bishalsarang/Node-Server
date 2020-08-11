66931135da096facfb798d3435331bff673f80ac
Bishalsarang <bishalsarang@gmail.com>
2020-08-11 07:22:44 +0545

    Remove temp files

--- src/fileOperation.js
+++ src/fileOperation.js
@@ -12,6 +12,18 @@ const read = (filePath, encoding = 'UTF-8') => {
   });
 };
 
+const rename = (filePath, newFilePath) => {
+  return new Promise((resolve, reject) => {
+    fs.rename(filePath, newFilePath, (err, done) => (err ? reject(err) : resolve(done)));
+  });
+};
+
+// const delete = (filePath) => {
+// return new Promise((resolve, reject) => {
+//   fs.(filePath, newFilePath, (err, done) => (err ? reject(err) : resolve(done)));
+// });
+// };
+
 /**
  *
  *@param {String} mode Mode to open file: read, write or append
@@ -21,14 +33,19 @@ const read = (filePath, encoding = 'UTF-8') => {
 const open = (filePath, mode, content = '', encoding = 'UTF-8') => {
   switch (mode.toLowerCase()) {
     case 'read':
-      read(filePath)
+      read(filePath, encoding)
         .then((data) => console.log('Succesfully read', data))
-        .catch((err) => console.log('Error', e));
+        .catch((err) => console.log('Error', err));
       break;
     case 'write':
       write(filePath, content)
         .then((data) => console.log(`Successfully written ${filePath}`))
-        .catch((err) => console.log('Error', e));
+        .catch((err) => console.log('Error', err));
+      break;
+    case 'rename':
+      rename(filePath, './newOut.txt')
+        .then(() => console.log('Successfully renamed'))
+        .catch((err) => console.log('Error', err));
       break;
     case 'append':
       console.log('append');
@@ -36,15 +53,14 @@ const open = (filePath, mode, content = '', encoding = 'UTF-8') => {
     case 'delete':
       console.log('delete');
       break;
-    case 'rename':
-      console.log('rename');
-      break;
+
     default:
       console.log('Operation not found');
   }
 };
 
-open('./out.txt', 'write', 'hello');
-open('./out.txt', 'read');
+// open('./out.txt', 'write', 'hello');
+// open('./out.txt', 'read');
+open('./out.txt', 'rename', './newOut.txt');
 console.log('DO stg');
-module.exports = { open };
+module.exports = { open, read, write, rename };
